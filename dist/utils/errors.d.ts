85513d64e5aa5e63674b5b8388d3a8b850659ad6
Pragun Saini <pragunsaini117@gmail.com>
2020-03-18 19:37:19 +0530

    Removed distribution files from Git

--- dist/utils/errors.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Custom Error class to identify and distinguish errors raised by the database
- */
-declare class DatabaseError extends Error {
-    code?: string;
-    constructor(code?: string, message?: string);
-}
-export { DatabaseError };
